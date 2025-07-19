e2ded068254d97e784367c2ceb252c39af670e9c
lowpez <sadscientist101@gmail.com>
2025-07-19 16:39:12 +0800

    make exception OutOfStockException for situations when product is out of stock

---
+++ src/main/java/com/lopez/ecommerce_api/exception/OutOfStockException.java
@@ -0,0 +1,8 @@
+package com.lopez.ecommerce_api.exception;
+
+public class OutOfStockException extends Exception {
+
+    public OutOfStockException(String message) {
+        super(message);
+    }
+}
